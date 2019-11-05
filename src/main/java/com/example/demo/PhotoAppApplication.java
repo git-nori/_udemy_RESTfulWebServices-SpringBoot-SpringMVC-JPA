0f6b441173b74735fc70acaba30aa2a44c6c560d
HironoriSato <maza.hs5360@gmail.com>
2019-11-05 22:54:39 +0900

    [add]PasswordEncoderをBean定義するよう設定

--- src/main/java/com/example/demo/PhotoAppApplication.java
+++ src/main/java/com/example/demo/PhotoAppApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class PhotoAppApplication {
@@ -10,4 +12,8 @@ public class PhotoAppApplication {
 		SpringApplication.run(PhotoAppApplication.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncorder() {
+	    return new BCryptPasswordEncoder();
+	}
 }
