3c0306408462d90054a3d4e6f6ace3f778bdb6d9
HironoriSato <maza.hs5360@gmail.com>
2019-11-07 17:42:49 +0900

    [mod]CurdRepository => PagingAndSortingRepositoryに変更

--- src/main/java/com/example/demo/io/repository/UserRepository.java
+++ src/main/java/com/example/demo/io/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package com.example.demo.io.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.io.entity.UserEntity;
 
 @Repository
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
+public interface UserRepository extends PagingAndSortingRepository<UserEntity, Long>{
     public UserEntity findByEmail(String email);
 
     public UserEntity findByUserId(String userId);
